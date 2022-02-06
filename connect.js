b3f9241f5e5a9a4e0ce95c1d76163eafaf08cc1c
mycomi <mycomi20th@gmail.com>
2022-02-06 18:07:44 +0700

    npm

--- connect.js
+++ connect.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
+const fs = require('fs');
 
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST ,
     user: process.env.DATABASE_USER ,
     password: process.env.DATABASE_PASSWORD ,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+    ssl: {ca: fs.readFileSync("./BaltimoreCyberTrustRoot.crt.pem")}
   });
 
 module.exports = db;
